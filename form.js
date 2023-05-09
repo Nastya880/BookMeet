5e642009ebc4f8335f7f1ba67691a2c8b3caf7cf
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2023-05-09 15:53:10 +0300

    refactor

--- form.js
+++ form.js
@@ -1,51 +1,55 @@
+//'use strict'
 // элементы с выпадающим списком
-let selectTower = document.getElementById("towerID");
-let selectFloor = document.getElementById("floorID");
-let selectNumberRoom = document.getElementById("numberRoomID");
+const selectElements = {
+  tower: document.getElementById("towerID"),
+  floor: document.getElementById("floorID"),
+  numberRoom: document.getElementById("numberRoomID"),
+  optionsTower: ["А", "Б"]  // содержимое выпадающего списка  для поля "Башня"
+}
 
-// содержимое выпадающего списка  для поля "Башня"
-const optionsTower = ["А", "Б"];
-
-let form = document.querySelector(".form");
-let comment = form.querySelector(".comment");
-let fields = form.querySelectorAll(".field");
-
-// проверка даты
-let dateChoose = form.querySelector(".dateChoose");
-let dateCheck = false;
-
-// проверка времени
-let timeStartChoose = form.querySelector(".timeStartChoose");
-timeStartChoose.value = "";
-let timeEndChoose = form.querySelector(".timeEndChoose");
-let timeStartCheck = false;
-let timeEndCheck = false;
+const form = document.querySelector(".form");
+const comment = form.querySelector(".comment");
+const fields = form.querySelectorAll(".field");
 
 // текущие дата и время
-let localDate = new Date().toLocaleDateString("ru-RU");
-let localTime = new Date().toTimeString("ru-Ru");
-let localMinute = localTime.slice(3,5);
-let localHour = localTime.slice(0,2);
-let localYear = localDate.slice(6,10);
-let localMonth = localDate.slice(3,5);
-let localDay = localDate.slice(0,2);
+const localDate = new Date().toLocaleDateString("ru-RU");
+const localTime = new Date().toTimeString("ru-Ru");
 
-let chooseYear;
-let chooseMonth;
-let chooseDay;
-let chooseStartMinute;
-let chooseStartHour;
+// проверка даты
+const checkInputDate = {
+  input: form.querySelector(".dateInput"),
+  dateCheck: false,
+  localYear: localDate.slice(6,10),
+  localMonth: localDate.slice(3,5),
+  localDay: localDate.slice(0,2)
+}
+
+// проверка времени
+const checkInputTime = {
+  startInput: form.querySelector(".timeStartInput"),
+  endInput: form.querySelector(".timeEndInput"),
+  startCheck: false,
+  endCheck: false,
+  localMinute: localTime.slice(3,5),
+  localHour: localTime.slice(0,2)
+}
+
+//checkInputTime.startInput.value = "";
 
 const clearButton = document.getElementById("clearButtonID");
 const sendButton = document.getElementById("sendButtonID");
 
+optionTower();
+optionFloor();
+optionNumberRoom();
+
 function optionTower()
 {
-  for (let i = 0; i < optionsTower.length; i++) {
+  for (let i = 0; i < selectElements.optionsTower.length; i++) {
     let elTower = document.createElement("option");
-    elTower.textContent = optionsTower[i];
-    elTower.value = optionsTower[i];
-    selectTower.appendChild(elTower);
+    elTower.textContent = selectElements.optionsTower[i];
+    elTower.value = selectElements.optionsTower[i];
+    selectElements.tower.appendChild(elTower);
   }
 }
 
@@ -55,7 +59,7 @@ function optionFloor()
     let elFloor = document.createElement("option");
     elFloor.textContent = i;
     elFloor.value = i;
-    selectFloor.appendChild(elFloor);
+    selectElements.floor.appendChild(elFloor);
   }
 }
 
@@ -65,7 +69,7 @@ function optionNumberRoom()
     let elNumberRoom = document.createElement("option");
     elNumberRoom.textContent = i;
     elNumberRoom.value = i;
-    selectNumberRoom.appendChild(elNumberRoom);
+    selectElements.numberRoom.appendChild(elNumberRoom);
   }
 }
 
@@ -86,90 +90,83 @@ function checkFieldsPresence () {
   for (let i = 0; i < fields.length; i++) {
     fields[i].style.color = "black";
     if (!fields[i].value)
-    {
       return (generateError("Поле не заполнено", fields[i], fields[i]));
-    }
   }
-  if (!dateCheck)
-    return (generateError("Неверное значение поля", dateChoose, dateChoose));
-  if (!timeStartCheck)
-    return (generateError("Неверное значение поля", timeStartChoose, timeStartChoose));
-  if (!timeEndCheck)
-    return (generateError("Неверное значение поля", timeEndChoose, timeEndChoose));
+  if (!checkInputDate.dateCheck)
+    return (generateError("Неверное значение поля", checkInputDate.input, checkInputDate.input));
+  if (!checkInputTime.startCheck)
+    return (generateError("Неверное значение поля", checkInputTime.startInput, checkInputTime.startInput));
+  if (!checkInputTime.endCheck)
+    return (generateError("Неверное значение поля", checkInputTime.endInput, checkInputTime.endInput));
   return true;
 }
 
 function clearForm(valueClear)
 {
-  console.log("Значение ", valueClear, " удалено");
   valueClear = "";
   return valueClear;
 }
 
-optionTower();
-optionFloor();
-optionNumberRoom();
-
 // проверка даты (выбрана текущая и позже)
-dateChoose.addEventListener("change", function (e) {
-  if(!dateCheck)
-    e.target.style.color = "black";
+checkInputDate.input.addEventListener("change", function (event) {
+  if(!checkInputDate.dateCheck)
+    event.target.style.color = "black";
   // выбранная пользователем дата
-  let chooseDateString = e.target.value.toString();
-  chooseYear = chooseDateString.slice(0,4);
-  chooseMonth = chooseDateString.slice(6,7);
-  chooseDay = chooseDateString.slice(8,10);
+  let inputDateString = event.target.value.toString();
+  checkInputDate.inputYear = inputDateString.slice(0,4);
+  checkInputDate.inputMonth = inputDateString.slice(6,7);
+  checkInputDate.inputDay = inputDateString.slice(8,10);
 
-  if(localYear - chooseYear > 0)
-    dateCheck = generateError("Неверный год", chooseYear,  e.target);
-  else if (localYear - chooseYear == 0 && localMonth - chooseMonth > 0)
-    dateCheck = generateError("Неверный месяц", chooseMonth,  e.target);
-  else if (localYear - chooseYear == 0 && localMonth - chooseMonth == 0 && localDay - chooseDay > 0)
-    dateCheck = generateError("Неверный день", chooseDay, e.target);
+  if(checkInputDate.localYear - checkInputDate.inputYear > 0)
+    checkInputDate.dateCheck = generateError("Неверный год", checkInputDate.inputYear, event.target);
+  else if (checkInputDate.localYear - checkInputDate.inputYear == 0 && checkInputDate.localMonth - checkInputDate.inputMonth > 0)
+    checkInputDate.dateCheck = generateError("Неверный месяц", checkInputDate.inputMonth, event.target);
+  else if (checkInputDate.localYear - checkInputDate.inputYear == 0 && checkInputDate.localMonth - checkInputDate.inputMonth == 0 && checkInputDate.localDay - checkInputDate.inputDay > 0)
+    checkInputDate.dateCheck = generateError("Неверный день", checkInputDate.inputDay, event.target);
   else {
-    e.target.style.color= "black";
-    dateCheck = true;
+    event.target.style.color = "black";
+    checkInputDate.dateCheck = true;
   }
 });
 
 // проверка времени начала (выбрана текущая и позже)
-timeStartChoose.addEventListener("change", function (e) {
-  if(!timeStartCheck)
-    e.target.style.color= "black";
+checkInputTime.startInput.addEventListener("change", function (event) {
+  if(!checkInputTime.startCheck)
+    event.target.style.color= "black";
   // выбранное пользователем время начала бронирования
-  let chooseTimeStartString = e.target.value.toString();
-  chooseStartMinute = chooseTimeStartString.slice(3,5);
-  chooseStartHour = chooseTimeStartString.slice(0,2);
-  if(!dateCheck)
-    timeStartCheck = generateError("Указана неверная дата", dateChoose, e.target);
-  else if (localYear - chooseYear == 0 && localMonth - chooseMonth == 0 && localDay - chooseDay == 0) {
-    if (localHour - chooseStartHour > 0)
-      timeStartCheck = generateError("Указано прошедшее время (час)", chooseStartHour, e.target);
-    else if (localHour - chooseStartHour == 0 && localMinute - chooseStartMinute > 0)
-      timeStartCheck = generateError("Указано прошедшее время (минуты)", chooseStartMinute, e.target);
+  let inputTimeStartString = event.target.value.toString();
+  checkInputTime.inputStartMinute = inputTimeStartString.slice(3,5);
+  checkInputTime.inputStartHour = inputTimeStartString.slice(0,2);
+  if(!checkInputDate.dateCheck)
+  checkInputTime.startCheck = generateError("Указана неверная дата", checkInputDate.input, event.target);
+  else if (checkInputDate.localYear - checkInputDate.inputYear == 0 && checkInputDate.localMonth - checkInputDate.inputMonth == 0 && checkInputDate.localDay - checkInputDate.inputDay == 0) {
+    if (checkInputTime.localHour - checkInputTime.inputStartHour > 0)
+      checkInputTime.startCheck = generateError("Указано прошедшее время (час)", checkInputTime.inputStartHour, event.target);
+    else if (checkInputTime.localHour - checkInputTime.inputStartHour == 0 && checkInputTime.localMinute - checkInputTime.inputStartMinute > 0)
+      checkInputTime.startCheck = generateError("Указано прошедшее время (минуты)", checkInputTime.inputStartMinute, event.target);
   } else {
-    e.target.style.color= "black";
-    timeStartCheck = true;
+    event.target.style.color= "black";
+    checkInputTime.startCheck = true;
   }
 });
 
 // проверка времени окончания (выбрана текущая и позже)
-timeEndChoose.addEventListener("change", function (e) {
-  if(!timeEndCheck)
-    e.target.style.color= "black";
+checkInputTime.endInput.addEventListener("change", function (event) {
+  if(!checkInputTime.endCheck)
+    event.target.style.color= "black";
   // выбранное пользователем время окончания бронирования
-  let chooseTimeEndString = e.target.value.toString();
-  let chooseEndMinute = chooseTimeEndString.slice(3,5);
-  let chooseEndHour = chooseTimeEndString.slice(0,2);
-  if(!timeStartCheck)
-    timeEndCheck = generateError("Указано неверное время начала бронирования переговорной", timeStartChoose, e.target);
-  else if (chooseStartHour - chooseEndHour > 0)
-    timeEndCheck = generateError("Время окончания бронирования должно быть после время начала (час)", timeEndChoose, e.target);
-  else if (chooseStartHour - chooseEndHour == 0 && chooseStartMinute - chooseEndMinute >= 0)
-    timeEndCheck = generateError("Время окончания бронирования должно быть после время начала (минуты)", timeEndChoose, e.target);
+  let inputTimeEndString = event.target.value.toString();
+  checkInputTime.inputEndMinute = inputTimeEndString.slice(3,5);
+  checkInputTime.inputEndHour = inputTimeEndString.slice(0,2);
+  if(!checkInputTime.startCheck)
+    checkInputTime.endCheck = generateError("Указано неверное время начала бронирования переговорной", checkInputTime.startInput, event.target);
+  else if (checkInputTime.inputStartHour - checkInputTime.inputEndHour > 0)
+    checkInputTime.endCheck = generateError("Время окончания бронирования должно быть после время начала (час)", checkInputTime.endInput, event.target);
+  else if (checkInputTime.inputStartHour - checkInputTime.inputEndHour == 0 && checkInputTime.inputStartMinute - checkInputTime.inputEndMinute >= 0)
+    checkInputTime.endCheck = generateError("Время окончания бронирования должно быть после время начала (минуты)", checkInputTime.endInput, event.target);
   else {
-    e.target.style.color= "black";
-    timeEndCheck = true;
+    event.target.style.color= "black";
+    checkInputTime.endCheck = true;
   }
 });
 
@@ -180,16 +177,19 @@ sendButton.addEventListener("click", function handleClick(event) {
   if (!checkFieldsPresence())
     console.log("Ошибка при отправке формы");
   else {
-    const Store_Form_Data = {}
-    Store_Form_Data.tower = selectTower.value;
-    Store_Form_Data.floor = selectFloor.value;
-    Store_Form_Data.numberRoom = selectNumberRoom.value;
-    Store_Form_Data.date = dateChoose.value;
-    Store_Form_Data.timeStart = timeStartChoose.value;
-    Store_Form_Data.timeEnd = timeEndChoose.value;
-    Store_Form_Data.comment = comment.value;
-    console.log("Форма отправлена\n", JSON.stringify(Store_Form_Data))
-    alert("Переговорная успешно забронирована")
+    const eventData = {
+      tower: selectElements.tower.value,
+      floor: +selectElements.floor.value, // превращаю в число
+      numberRoom: +selectElements.numberRoom.value, // превращаю в число
+      date: checkInputDate.input.value,
+      timeStart: checkInputTime.startInput.value,
+      timeEnd: checkInputTime.endInput.value,
+      comment: comment.value
+    }
+
+    console.log("Форма отправлена\n", JSON.stringify(eventData));
+    console.log("Поля формы очищены после отправки формы");
+    alert("Переговорная успешно забронирована");
     for (let i = 0; i < fields.length; i++) {
       fields[i].style.color = "black";
       if (fields[i].value)
@@ -205,6 +205,7 @@ clearButton.addEventListener('click', function handleClick(event) {
   for (let i = 0; i < fields.length; i++) {
     fields[i].style.color = "black";
     if (fields[i].value)
+      console.log("Значение ", fields[i].value, " удалено");
       fields[i].value = clearForm(fields[i].value);
   }
   alert("Значения полей формы удалены");
